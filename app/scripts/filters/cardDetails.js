3886d949030a65320ddb5b2c3fb829b29093df67
Matthew Agius <mtagius@gmail.com>
2019-02-22 21:18:44 -0500

    my trimmed down self hosted version

--- app/scripts/filters/cardDetails.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('mpk').filter('cardDetails', function () {
-	return function (input) {
-		if (input == undefined || input === '') return input;
-		return input.replace(/&#10;/g, "<br />");
-	};
-});
--- app/scripts/services/fileService.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('mpk').factory('fileService', function () {
-	return {
-		saveBlob: function(blob, fileName){
-			return saveAs(blob, fileName);
-		}
-	};
-});
